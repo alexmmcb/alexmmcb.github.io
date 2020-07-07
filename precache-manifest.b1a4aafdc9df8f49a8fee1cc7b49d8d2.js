f8a4426dd72a5208ec8a39c8020b1aa13f897c30
alexmmcb <molinamora.alex@gmail.com>
2020-07-07 20:52:01 +0200

    first build

---
+++ precache-manifest.b1a4aafdc9df8f49a8fee1cc7b49d8d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce97a56e3c02e12e8bf34d84d56d1c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fdd7a84465372b102ed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d524cf516c8e0dab2d5",
+    "url": "/static/js/2.e5e2e3d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5e2e3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdd7a84465372b102ed",
+    "url": "/static/js/main.e5ea45f8.chunk.js"
+  },
+  {
+    "revision": "2c1194155071dd44dba2",
+    "url": "/static/js/runtime-main.6f4c2e45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
